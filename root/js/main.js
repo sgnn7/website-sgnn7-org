11baa40ff7fad8e1e5e09b90c38002fa1fe9c20c
Srdjan Grubor <sgnn7@sgnn7.org>
2015-08-08 14:10:12 -0500

    Added email adress link
    Why not?

--- root/js/main.js
+++ root/js/main.js
@@ -59,7 +59,8 @@ var Commands = function(hostline, user, group) {
                                output: 'drwx------ 113 ' + formatSpaces(user, 5) + ' ' + formatSpaces(group, 5) + ' 36864 Jul 24 22:38 .\n' +
                                        'drwxr-xr-x   6 ' + formatSpaces('root', 5) + ' ' + formatSpaces('root', 5) + '  4096 Jan  6  2015 ..\n' +
                                        softLink(user, group, 'github', 'https://github.com/sgnn7') + '\n' +
-                                       softLink(user, group, 'twitter', 'https://twitter.com/sgnn7')
+                                       softLink(user, group, 'twitter', 'https://twitter.com/sgnn7') + '\n' +
+                                       softLink(user, group, 'e-mail', 'mailto:sgnn7@sgnn7.org')
                              },
              'dd_partition': { typedCommand: 'dd if=/dev/urandom of=/dev/' + dd_device + ' bs=' + dd_block_size + 'M',
                                startDelay: 1200,
